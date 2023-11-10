8eab3cc4018f25735ca1c6a66c445fba455179c5
Kai Wu(吳玉凱) <kai.wu@tenmax.io>
2023-11-10 17:52:13 +0800

    some message > > Co-authored-by: yukai1023 <wuyukai1023work@gmail.com> Co-authored-by: Kai19971023 <wuyukai1023@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            123dsdsd
+            123dsdsdsdsd
           </Link>
         </div>
       </div>
