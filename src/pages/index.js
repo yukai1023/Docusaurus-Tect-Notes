58f949b294bd6f8596fdd5efb0797950d75f3f24
Yukai <91178978+yukai1023@users.noreply.github.com>
2023-11-10 18:15:39 +0800

    test (#4)
    * test
    
    * Update index.js
    
    ---------
    
    Co-authored-by: Kai Wu(吳玉凱) <kai.wu@tenmax.io>
    Co-authored-by: Kai19971023 <150427481+Kai19971023@users.noreply.github.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            123dsdsdsdsd
+            123dsdsdsdsdsddqdq123213213123213
           </Link>
         </div>
       </div>
