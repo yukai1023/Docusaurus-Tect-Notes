47c5a97fe94a3200ffb077a7070aa1b312492ecc
Kai Wu(吳玉凱) <kai.wu@tenmax.io>
2023-11-10 18:08:57 +0800

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            123dsdsdsdsd
+            123dsdsdsdsdsddqdq
           </Link>
         </div>
       </div>
